f90aa95cea9beb29789f344e8c2790ed784cbcb1
manchesterunited5 <nguyencaotoan534@gmail.com>
2024-12-10 10:44:27 +0700

    company

---
+++ VehicleShowroom.Management.UI/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using VehicleShowroom.Mangement.Application.Abstracts;
+using VehicleShowroomManagementSystem.Areas.Admin.Controllers;
+
+namespace VehicleShowroom.Management.UI.Areas.Admin.Controllers
+{
+    public class CompanyController : BaseController
+    {
+        private readonly ICompanyServices _companyServices;
+        private readonly IMapper _mapper;
+        public CompanyController(ICompanyServices companyServices, IMapper mapper)
+        {
+            _companyServices = companyServices;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index(string? keyword, int? page = 1)
+        {
+            ViewBag.keyword = keyword;
+            ViewData["CurrentPage"] = page;
+            var data = await _companyServices.GetAllPaginationAsync(keyword, page ?? 1, 8);
+            return View(data);
+        }
+    }
+}
---
+++ VehicleShowroom.Mangement.Application/Abstracts/ICompanyServices.cs
@@ -0,0 +1,16 @@
+﻿
+using VehicleShowroom.Mangement.Application.Models.DTOs;
+using VehicleShowroom.Mangement.Application.Models.ViewModels;
+using VehicleShowroom.Mangement.Domain.Entities;
+using X.PagedList;
+
+namespace VehicleShowroom.Mangement.Application.Abstracts
+{
+    public interface ICompanyServices
+    {
+        Task<IPagedList<CompanyDTO>> GetAllPaginationAsync(string keyword, int page, int pageSize = 8);
+        Task<bool> DeleteAsync(int id);
+        Task<Company> GetByIdAsync(int id);
+        Task<bool> SaveOrUpdateAsync(CompanyViewModel model);
+    }
+}
